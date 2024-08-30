52d7f4c7a9c380eef962c27b0ccf08781b7c2d8c
RobertLikesCoding <rbaufeld@posteo.de>
2024-08-30 07:54:52 +0200

    moved AccessToken Fetch to App.js for fetch on page load

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,32 +4,8 @@ import styles from '../styles/SearchBar.module.css';
 export default function SearchBar() {
   const [search, setSearch] = useState('');
 
-  // Fetching the access token
-  fetch("https://accounts.spotify.com/api/token", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      'grant-type': 'client_credentials',
-      'client-id': '9176b7ef419541fea4e1b9e3aef00813',
-      'client-secret': '4c59a512f5064dbda406daf9d782afdf'
-    })
-    }).then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch token');
-      } else {
-        return response.json();
-      }}).then(data => {
-        const accessToken = data.access_token;
-        console.log(data);
-      }).catch(error => {
-        console.error(error);
-      });
-
   function handleSearch(event) {
     setSearch(event.target.value);
-
   }
 
   return (
