3426859dd99067cfe430219f78baf4cc170cfcfb
Robert Baufeld <rbaufeld@posteo.de>
2024-09-03 16:32:34 +0200

    added input element for playlist name

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,10 +2,16 @@ import React, {useState} from 'react';
 import Track from './Track';
 
 export default function Playlist({playlistTracks, handleRemove}) {
+  const [playlistName, setPlaylistName] = useState('');
+
+  function handleChange({target}) {
+    setPlaylistName(target.value)
+  }
 
   return (
     <div className='playlist'>
       <h2>Playlist</h2>
+      <input onChange={handleChange}/>
       {playlistTracks.map((track) => {
         return <Track track={track} key={track.id} addOrRemove='remove' onClick={(e) => handleRemove(track)}/>
       })}
