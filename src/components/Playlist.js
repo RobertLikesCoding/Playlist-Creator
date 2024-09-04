97dd0daaef9892c0aaa3270bfce11cdbd4a5bcc6
Robert Baufeld <rbaufeld@posteo.de>
2024-09-04 11:07:08 +0200

    minor changes

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -8,13 +8,25 @@ export default function Playlist({playlistTracks, handleRemove}) {
     setPlaylistName(target.value)
   }
 
+  function handleSubmit(e) {
+    playlistTracks.map((track) => {
+      return track.id
+    })
+  }
+
   return (
     <div className='playlist'>
       <h2>Playlist</h2>
-      <input onChange={handleChange}/>
-      {playlistTracks.map((track) => {
-        return <Track track={track} key={track.id} addOrRemove='remove' onClick={(e) => handleRemove(track)}/>
-      })}
+      <form>
+        <label for='playlistName'>Name your playlist: </label>
+        <input name='playlistName' type='text' onChange={handleChange}/>
+        <ul>
+          {playlistTracks.map((track) => {
+            return <Track track={track} key={track.id} addOrRemove='remove' onClick={(e) => handleRemove(track)}/>
+          })}
+        </ul>
+        <button type='submit' onClick={handleSubmit}>Save to Spotify</button>
+      </form>
     </div>
   )
 }
